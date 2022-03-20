f296d059409509b3a49090f4dfde4dcea1573ad4
AleksandrDenis <deniskudriavcev@gmail.com>
2022-03-20 18:16:38 +0000

    Remove print statment

--- run.py
+++ run.py
@@ -7,15 +7,14 @@ print("Hello, " + players_name + " and best of luck!")
 
 def get_word(fname):
     """
-    Get word from list
+    Get word from list 
     Choose random word 
     """
-    word_list = open('wordlist.txt','r+')
+    word_list = open('wordlist.txt', 'r+')
     word = random.choice(word_list.read().split())
     return word.upper()
 
 word = get_word('wordlist.txt')
-print(word)
 
 def word_underscore():
     """
@@ -35,6 +34,7 @@ word_guessed = list(word_underscore)
 
 while mistakes_allowed > 0:
     print("You only allowed " + str(mistakes_allowed) + " mistakes")
+    print("\n")
     guess = input("Please " + players_name + " guess letter: ").upper()
     if guess in letters_guessed:
         print("You alredy guessed the letter", guess)
@@ -49,10 +49,8 @@ while mistakes_allowed > 0:
                 word_guessed[i] = guess
         print(''.join(word_guessed))
         letters_guessed.append(guess)
-        
     if ''.join(word_guessed) == word:
         print("Congratulations! " + players_name + " you guessed the word!")
         break
     elif mistakes_allowed == 0:
-        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
-        
+        print("Sorry, you ran out of tries. The word was " + word)        
