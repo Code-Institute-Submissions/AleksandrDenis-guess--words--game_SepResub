34db9809bc625fc4f2f23fef2a4519e89e3768de
AleksandrDenis <deniskudriavcev@gmail.com>
2022-03-20 16:33:08 +0000

    Add print to check that word is selected randomly from list and printed in uppercase

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 import random
 
 def get_word(fname):
+    """
+    Get word from list
+    Choose random word 
+    """
     word_list = open('wordlist.txt','r+')
     word = random.choice(word_list.read().split())
     return word.upper()
+word = get_word('wordlist.txt')
+print(word)
     
