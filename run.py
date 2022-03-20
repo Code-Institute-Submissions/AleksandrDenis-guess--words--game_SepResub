7cd6f9794cb11d947b6629943150b188f7954397
AleksandrDenis <deniskudriavcev@gmail.com>
2022-03-20 17:21:39 +0000

    Add print function to welcome user and input to get user name

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import random
 
+print("Welcome to word guessing game!")
+players_name = input("Before we start please tell me your name: ")
+print("\n")
+print("Hello, " + players_name + " and best of luck!")
+
 def get_word(fname):
     """
     Get word from list
@@ -8,6 +13,7 @@ def get_word(fname):
     word_list = open('wordlist.txt','r+')
     word = random.choice(word_list.read().split())
     return word.upper()
+
 word = get_word('wordlist.txt')
 print(word)
 
@@ -19,4 +25,10 @@ def word_underscore():
     word_underscore = []
     for i in range(len(word)):
         word_underscore.append('_')
-        return ''.join(word_underscore)
+    return ''.join(word_underscore)
+word_underscore = word_underscore()
+print(word_underscore)
+
+mistakes_allowed = 6
+letters_guessed = []
+word_guessed = list(word_underscore)
