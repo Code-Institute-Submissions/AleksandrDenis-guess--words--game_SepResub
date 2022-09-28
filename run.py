c9b329532fcad8770461c6718b2e17769407a37d
DenisKudriavcev <deniskudriavcev@gmail.com>
2022-09-28 18:50:09 +0000

    Change name word to random_word for better naming convention

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
-print("Welcome to word guessing game fruit edition!")
+# greating
+print("Welcome to word guessing game fruit edition!") 
 print("----------------------------------------")
 print("Game will randomly select a Mystery Word.\n",
       "You will have six chances to guess what it\n",
@@ -16,11 +17,11 @@ def get_word(fname):
     Choose random word 
     """
     word_list = open('wordlist.txt', 'r+')
-    word = random.choice(word_list.read().split())
-    return word.upper()
+    random_word = random.choice(word_list.read().split())
+    return random_word.upper()
 
 
-word = get_word('wordlist.txt')
+random_word = get_word('wordlist.txt')
 
 
 def word_underscore():
@@ -29,7 +30,7 @@ def word_underscore():
     Same length as mystery word
     """
     word_underscore = []
-    for i in range(len(word)):
+    for i in range(len(random_word)):
         word_underscore.append('_')
     return ''.join(word_underscore)
 
@@ -47,19 +48,19 @@ while mistakes_allowed > 0:
     guess = input("Please " + players_name + " guess letter:\n ").upper()
     if guess in letters_guessed:
         print("You alredy guessed the letter", guess)
-    elif guess not in word:
+    elif guess not in random_word:
         print(guess, " is not in the word.")
         mistakes_allowed -= 1
         letters_guessed.append(guess)
     else:
         print("Weldone " + players_name + " " + guess + " is in the word.")
-        for i in range(len(word)):
-            if list(word)[i] == guess:
+        for i in range(len(random_word)):
+            if list(random_word)[i] == guess:
                 word_guessed[i] = guess
         print(''.join(word_guessed))
         letters_guessed.append(guess)
-    if ''.join(word_guessed) == word:
+    if ''.join(word_guessed) == random_word:
         print("Congratulations! " + players_name + " you guessed the word!")
         break
     elif mistakes_allowed == 0:
-        print("Sorry, you ran out of tries. The word was " + word)        
+        print("Sorry, you ran out of tries. The word was " + random_word)        
